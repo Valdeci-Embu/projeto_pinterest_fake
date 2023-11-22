78e2f4518b6518e9c55884ef8ad431f5e80292c4
Valdeci <valdecie@gmail.com>
2023-11-22 13:44:23 -0300

    adicionei README e requirements

---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto criado como atividade prática do curso Python impressionador da Hashtag Treinamentos.
+O projeto consiste na criação de um site que simula o Pinterest, usando Flask
--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+#a linha abaixo era pra usar bd local
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+#app.config["SQLALCHEMY_DATABASE_URI"] = postgresql://projeto_pinterest_user:7p0m1Sdq4Zjz1Oy8Tqc7A6BhhWEyH27l@dpg-clf280fjc5ks73dgu110-a.oregon-postgres.render.com/projeto_pinterest
+# Para criar o bd la no Render
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL") #essa linha é do bd postgresql do Render
 app.config["SECRET_KEY"] = "36935f480034d7a10c5cb587ea7803f7"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
